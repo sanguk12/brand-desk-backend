d765405504802db672da7a887a1c9b8e315dcf4d
poh <qudansdl@gmail.com>
2022-06-02 10:25:55 +0900

    등록 오류 수정

--- biz-core/src/main/java/com/samsung/ds/controller/api/RequestController.java
+++ biz-core/src/main/java/com/samsung/ds/controller/api/RequestController.java
@@ -1,24 +1,23 @@
 package com.samsung.ds.controller.api;
 
 import com.samsung.ds.views.pojo.model.ReviewRequest;
+import com.synccms.common.constants.CommonConstants;
 import com.synccms.common.handler.PageHandler;
 import com.synccms.common.pojo.AjaxResponse;
-import com.synccms.entities.cms.CmsCategory;
-import com.synccms.entities.cms.CmsDictionary;
-import com.synccms.entities.cms.CmsDictionaryItem;
+import com.synccms.common.tools.CmsFileUtils;
+import com.synccms.common.tools.ExtendUtils;
+import com.synccms.common.tools.RequestUtils;
+import com.synccms.entities.cms.*;
 import com.synccms.entities.sys.SysSite;
 import com.synccms.entities.sys.SysUser;
-import com.synccms.logic.service.cms.CmsCategoryService;
-import com.synccms.logic.service.cms.CmsContentService;
-import com.synccms.logic.service.cms.CmsDictionaryItemService;
-import com.synccms.logic.service.cms.CmsDictionaryService;
+import com.synccms.logic.service.cms.*;
 import com.synccms.views.pojo.query.CmsContentQuery;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 /**
  *
@@ -30,6 +29,8 @@ import java.util.List;
 public class RequestController {
     protected static final Log log = LogFactory.getLog(RequestController.class);
     @Autowired private CmsContentService service;
+    @Autowired private CmsContentFileService fileService;
+    @Autowired private CmsContentAttributeService attributeService;
     @Autowired private CmsCategoryService categoryService;
 
     @Autowired private CmsDictionaryService dictionaryService;
@@ -61,8 +62,54 @@ public class RequestController {
      * @return view name
      */
     @RequestMapping(value = "write", method = RequestMethod.POST)
-    public AjaxResponse write(@RequestAttribute SysSite site, @RequestBody ReviewRequest req) {
-        return AjaxResponse.success();
+    public AjaxResponse write(
+            @RequestAttribute SysSite site,
+            @SessionAttribute SysUser user,
+            @RequestBody ReviewRequest req) {
+        CmsCategory cate = categoryService.getEntityByCode(site.getId(), "REQUEST");
+
+        CmsContent entity = new CmsContent();
+        entity.setSiteId(site.getId());
+        entity.setUserId(user.getId());
+        entity.setModelId("REQUEST");
+        entity.setTitle(req.getTitle());
+        entity.setCategoryId(cate.getId());
+        entity.setHasFiles(true);
+        entity.setPublishDate(new Date());
+        entity.setCheckDate(new Date());
+        entity.setSort(0);
+
+        entity.setStatus(CmsContentService.STATUS_NORMAL);
+
+        service.save(entity);
+
+        CmsContentAttribute attribute = new CmsContentAttribute();
+        attribute.setContentId(entity.getId());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("type", req.getType1() + "," + req.getType2());
+        map.put("content", req.getContent());
+        map.put("status", req.isTemp() ? "TEMPORARY" : "REQUEST");
+
+        attribute.setData(ExtendUtils.getExtendString(map));
+
+        attributeService.save(attribute);
+
+        for(int i = 0; i < req.getFiles().length; i++)
+        {
+            String filePath = req.getFiles()[i];
+            CmsContentFile file = new CmsContentFile();
+
+            file.setFilePath(filePath);
+            file.setFileType(CmsFileUtils.FILE_TYPE_OTHER);
+            file.setSort(i);
+            file.setUserId(user.getId());
+            file.setContentId(entity.getId());
+
+            fileService.save(file);
+        }
+
+        return AjaxResponse.success(entity.getId());
     }
 
 
