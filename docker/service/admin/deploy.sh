ac2e5923d571c8f8586c6b88c2114061e15a45d8
poh <qudansdl@gmail.com>
2022-06-10 15:39:31 +0900

    add docker build for web ui

---
+++ docker/service/admin/deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+docker-compose --env-file ./.env -f docker-compose.yml -p admin down
+docker system prune -f
+docker rmi registry.basicit.co.kr/branddesk/admin-backend-${BUILD_PROFILE}
+docker rmi registry.basicit.co.kr/branddesk/admin-ui-${BUILD_PROFILE}
+
+docker login registry.basicit.co.kr --username $REGISTRY_USERNAME --password  $REGISTRY_PASSWORD
+for run in {1..10}; do
+  if docker pull registry.basicit.co.kr/branddesk/admin-backend-${BUILD_PROFILE}; then
+      echo "success"
+      break
+  else
+      echo "Error Sleeping 2 seconds"
+      sleep 2
+  fi
+
+    if docker pull registry.basicit.co.kr/branddesk/admin-ui-${BUILD_PROFILE}; then
+        echo "success"
+        break
+    else
+        echo "Error Sleeping 2 seconds"
+        sleep 2
+    fi
+done
+
+HOSTNAME=`hostname -f`
+docker-compose --env-file ./.env -f docker-compose.yml -p admin up -d
+
